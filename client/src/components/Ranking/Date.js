34a0abf0825fa5e96d72d794925fe39776dcf1bb
Svaagi <jozefsvagerkom5@gmail.com>
2021-10-09 00:28:22 +0200

    ugly chart

---
+++ client/src/components/Ranking/Date.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+export default function Date() {
+  const [monthNumber, setDate] = useState(0)
+  const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
+  
+  const handleChange = (sign) => {
+    setDate(Math.abs(monthNumber + sign) % 12)
+  }
+  
+  return (
+    <div style={{fontSize:"10rem"}}>
+      <button onClick={() => handleChange(-1)}>----</button>
+      {month[monthNumber]}
+      <button onClick={() => handleChange(1)}>----</button>
+    </div>
+  )
+}
