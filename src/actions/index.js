dd1f300e20b2e4269eb2ce80c7508b2f7702b87d
LonziSlovakia <lonzikormos@gmail.com>
2021-10-08 14:20:11 +0000

    redux template with files

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+
+//
+//  REDUCER: SHOW STATE
+//
+
+// export const addTodo = text => ({
+//   type: 'ADD_TODO',
+//   payload: {
+//     text
+//   }
+// })
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// import { connect } from 'react-redux';
+// import * as actions from '../actions';
+export default function Home() {
+  return (
+    <>
+    </>
+  );
+}
+
+// const mapStateToProps = state => ({
+//   color: state.color
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   addTodo: text => dispatch(actions.addTodo(text)),
+// });
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(Home);
